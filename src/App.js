ed5f3eabea8c4d3b16d840f1b9c5544a23915862
Zac McClung <zac.mcclung370@gmail.com>
2018-05-14 21:14:30 -0600

    firebase setup

--- src/App.js
+++ src/App.js
@@ -2,9 +2,21 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 import reducers from './reducers';
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyAEd93UIPltgJCh2N7PbQisnxxHPypS8Hk',
+      authDomain: 'employeemanager-76c5c.firebaseapp.com',
+      databaseURL: 'https://employeemanager-76c5c.firebaseio.com',
+      projectId: 'employeemanager-76c5c',
+      storageBucket: '',
+      messagingSenderId: '701672200053'
+    };
+    firebase.initializeApp(config);
+  }
   render() {
     return (
       <Provider store={createStore(reducers)}>
