fd0387bc2bde4116f62706dc0d2203fd40694990
Zac McClung <zac.mcclung370@gmail.com>
2018-05-14 21:07:21 -0600

    finally gettin shizz running

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Text>Zac is a boss</Text>
+        </View>
+      </Provider>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
+
+export default App;
