8933f28e90bb563ffaf98ef373333120727a8cc4
Zac McClung <zac.mcclung370@gmail.com>
2018-05-15 23:21:49 -0600

    grouping scenes with buckets

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import firebase from 'firebase';
+import { Actions } from 'react-native-router-flux';
 import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS, LOGIN_USER_FAIL, LOGIN_USER } from './types';
 
 export const emailChanged = text => {
@@ -44,4 +45,6 @@ const loginUserSuccess = (dispatch, user) => {
     type: LOGIN_USER_SUCCESS,
     payload: user
   });
+  // employeeList() comes from scene key
+  Actions.main();
 };
