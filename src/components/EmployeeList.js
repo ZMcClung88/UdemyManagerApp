14c45c5a477c68f09dc12db5a5ede1e0b170f590
Zac McClung <zac.mcclung370@gmail.com>
2018-05-15 22:59:53 -0600

    displaying multiple scenes

---
+++ src/components/EmployeeList.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'React';
+import { View, Text } from 'react-native';
+
+class EmployeeList extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Employee List</Text>
+        <Text>Employee List</Text>
+        <Text>Employee List</Text>
+        <Text>Employee List</Text>
+        <Text>Employee List</Text>
+        <Text>Employee List</Text>
+      </View>
+    );
+  }
+}
+
+export default EmployeeList;
