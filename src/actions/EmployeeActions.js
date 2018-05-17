dc237e5329f35388197d50e3a5685723ad27ccef
Zac McClung <zac.mcclung370@gmail.com>
2018-05-17 17:42:28 -0600

    successfully save data to firebase

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,4 +1,6 @@
-import { EMPLOYEE_UPDATE } from './types';
+import firebase from 'firebase';
+import { Actions } from 'react-native-router-flux';
+import { EMPLOYEE_UPDATE, EMPLOYEE_CREATE, EMPLOYEES_FETCH_SUCCESS } from './types';
 
 export const employeeUpdate = ({ prop, value }) => {
   return {
@@ -8,5 +10,12 @@ export const employeeUpdate = ({ prop, value }) => {
 };
 
 export const employeeCreate = ({ name, phone, shift }) => {
-  console.log(name, phone, shift);
+  const { currentUser } = firebase.auth();
+
+  return dispatch => {
+    firebase
+      .database()
+      .ref(`/users/${currentUser.uid}/employees`)
+      .push({ name, phone, shift });
+  };
 };
