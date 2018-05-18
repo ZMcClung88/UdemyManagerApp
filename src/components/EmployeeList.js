56703c812eb70613efef0d0bb06b068c188b6afd
Zac McClung <zac.mcclung370@gmail.com>
2018-05-17 22:49:18 -0600

    list builing in employee list

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { ListView, View, Text } from 'react-native';
 import { employeesFetch } from '../actions';
+import ListItem from './ListItem';
 
 class EmployeeList extends Component {
   componentWillMount() {
@@ -27,18 +28,12 @@ class EmployeeList extends Component {
     this.dataSource = ds.cloneWithRows(employees);
   }
 
+  renderRow(employee) {
+    return <ListItem employee={employee} />;
+  }
+
   render() {
-    console.log('props', this.props);
-    return (
-      <View>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-      </View>
-    );
+    return <ListView enableEmptySections dataSource={this.dataSource} renderRow={this.renderRow} />;
   }
 }
 
